4a45f84936f50a203202c3d62bc79d7e109ab32f
madurohomero <homeromaduro@hotmail.com>
2020-06-22 14:11:30 -0300

    Login

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -22,73 +22,78 @@ class LoginScreen extends StatelessWidget {
           margin: const EdgeInsets.symmetric(horizontal: 16),
           child: Form(
             key: formKey,
-            child: ListView(
-              padding: const EdgeInsets.all(16),
-              shrinkWrap: true,
-              children: <Widget>[
-                TextFormField(
-                  controller: emailController,
-                  decoration: const InputDecoration(hintText: 'E-mail'),
-                  keyboardType: TextInputType.emailAddress,
-                  autocorrect: false,
-                  validator: (email) {
-                    if (!emailValid(email)) return 'E-mail inválido';
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextFormField(
-                  controller: passController,
-                  decoration: const InputDecoration(hintText: 'Senha'),
-                  autocorrect: false,
-                  obscureText: true,
-                  validator: (pass) {
-                    if (pass.isEmpty || pass.length < 6)
-                      return 'Senha Inválida';
-                    return null;
-                  },
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: FlatButton(
-                      onPressed: () {},
-                      padding: EdgeInsets.zero,
-                      child: const Text('Esqueci minha senha')),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                SizedBox(
-                  height: 44,
-                  child: RaisedButton(
-                    onPressed: () {
-                      if (formKey.currentState.validate()) {
-                        context.read<UserManager>().signIn(
-                            user: User(
-                                email: emailController.text,
-                                password: passController.text),
-                            onFail: (e) {
-                              scaffoldKey.currentState.showSnackBar(SnackBar(
-                                content: Text('Falha ao Entrar: $e'),
-                                backgroundColor: Colors.red,
-                              ));
-                            },
-                            onSuccess: () {
-                              // TODO : FECHAR A TELA DE LOGIN
-                            });
-                      }
-                    },
-                    color: Theme.of(context).primaryColor,
-                    textColor: Colors.white,
-                    child: Text(
-                      'Entrar',
-                      style: TextStyle(fontSize: 18),
+            child: Consumer<UserManager>(
+              builder: (_, userManager, __) {
+                return ListView(
+                  padding: const EdgeInsets.all(16),
+                  shrinkWrap: true,
+                  children: <Widget>[
+                    TextFormField(
+                      controller: emailController,
+                      decoration: const InputDecoration(hintText: 'E-mail'),
+                      keyboardType: TextInputType.emailAddress,
+                      autocorrect: false,
+                      validator: (email) {
+                        if (!emailValid(email)) return 'E-mail inválido';
+                        return null;
+                      },
                     ),
-                  ),
-                )
-              ],
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    TextFormField(
+                      controller: passController,
+                      decoration: const InputDecoration(hintText: 'Senha'),
+                      autocorrect: false,
+                      obscureText: true,
+                      validator: (pass) {
+                        if (pass.isEmpty || pass.length < 6)
+                          return 'Senha Inválida';
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    SizedBox(
+                      height: 44,
+                      child: RaisedButton(
+                        onPressed: () {
+                          if (formKey.currentState.validate()) {
+                            context.read<UserManager>().signIn(
+                                user: User(
+                                    email: emailController.text,
+                                    password: passController.text),
+                                onFail: (e) {
+                                  scaffoldKey.currentState
+                                      .showSnackBar(SnackBar(
+                                    content: Text('Falha ao Entrar: $e'),
+                                    backgroundColor: Colors.red,
+                                  ));
+                                },
+                                onSuccess: () {
+                                  // TODO : FECHAR A TELA DE LOGIN
+                                });
+                          }
+                        },
+                        color: Theme.of(context).primaryColor,
+                        textColor: Colors.white,
+                        child: Text(
+                          'Entrar',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    )
+                  ],
+                );
+              },
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.zero,
+                    child: const Text('Esqueci minha senha')),
+              ),
             ),
           ),
         ),
