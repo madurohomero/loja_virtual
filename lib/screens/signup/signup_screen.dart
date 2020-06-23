1bd0e25881a2649e364546dd3ba257de11c44a88
madurohomero <homeromaduro@hotmail.com>
2020-06-23 08:30:58 -0300

    signup_screen

--- lib/screens/signup/signup_screen.dart
+++ lib/screens/signup/signup_screen.dart
@@ -1,11 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/helpers/validators.dart';
 
 class SignUpScreen extends StatelessWidget {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Criar Conta'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Card(
+          margin: const EdgeInsets.symmetric(horizontal: 16),
+          child: Form(
+                      child: ListView(
+              padding: const EdgeInsets.all(16),
+              shrinkWrap: true,
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Nome Completo'),
+                  validator: (name) {
+                    if (name.isEmpty)
+                      return 'Campo obrigatório';
+                    else if (name.trim().split(' ').length <= 1)
+                      return 'Preencha seu Nome Completo';
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'E-mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (email) {
+                    if (email.isEmpty)
+                      return 'Campo obrigatório';
+                    else if (!emailValid(email)) return 'E-mail inválido';
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Senha'),
+                  obscureText: true,
+                  validator: (pass) {
+                    if (pass.isEmpty)
+                      return 'Campo obrigatório';
+                    else if (pass.length < 6) return 'Senha muito curta';
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Repita a Senha'),
+                  obscureText: true,
+                  validator: (pass) {
+                    if (pass.isEmpty)
+                      return 'Campo obrigatório';
+                    else if (pass.length < 6) return 'Senha muito curta';
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                  height: 44,
+                  child: RaisedButton(
+                    color: Theme.of(context).primaryColor,
+                    disabledColor: Theme.of(context).primaryColor.withAlpha(100),
+                    textColor: Colors.white,
+                    onPressed: () {},
+                    child: const Text(
+                      'Criar Conta',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
