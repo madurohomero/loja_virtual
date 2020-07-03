9f6be9210c7325ce8b0c46e8603f597e3fc5c411
madurohomero <homeromaduro@hotmail.com>
2020-07-03 14:17:13 -0300

    Pesquisa Produtos

--- lib/screens/products/products_screen.dart
+++ lib/screens/products/products_screen.dart
@@ -4,6 +4,8 @@ import 'package:loja_virtual/models/product_manager.dart';
 import 'package:loja_virtual/screens/products/components/product_list_tile.dart';
 import 'package:provider/provider.dart';
 
+import 'components/search_dialog.dart';
+
 class ProductsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,12 +14,25 @@ class ProductsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Produtos'),
         centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () async {
+                final search = await showDialog<String>(
+                    context: context, builder: (_) => SearchDialog());
+                if (search != null) {
+                  context.read<ProductManager>().search = search;
+                }
+              })
+        ],
       ),
       body: Consumer<ProductManager>(builder: (_, productManager, __) {
+        final filteredProducts = productManager.filteredProducts;
         return ListView.builder(
-          itemCount: productManager.allProducts.length,
+          padding: const EdgeInsets.all(4),
+          itemCount: filteredProducts.length,
           itemBuilder: (_, index) {
-            return ProductListTile(productManager.allProducts[index]);
+            return ProductListTile(filteredProducts[index]);
           },
         );
       }),
