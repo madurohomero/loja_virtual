b2a891a7ea135a53d3d1ba3215153dca55a2f5ec
madurohomero <homeromaduro@hotmail.com>
2020-08-02 23:44:28 -0300

    editando imagem

--- lib/screens/edit_product/components/image_source_sheet.dart
+++ lib/screens/edit_product/components/image_source_sheet.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImageSourceSheet extends StatelessWidget {
+  ImageSourceSheet({this.onImageSelected});
+  final Function(File) onImageSelected;
   final ImagePicker picker = ImagePicker();
   @override
   Widget build(BuildContext context) {
@@ -16,14 +18,18 @@ class ImageSourceSheet extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             FlatButton(
-              onPressed: () {
-                picker.getImage(source: ImageSource.camera);
+              onPressed: () async {
+                final PickedFile file =
+                    await picker.getImage(source: ImageSource.camera);
+                onImageSelected(File(file.path));
               },
               child: const Text('Câmera'),
             ),
             FlatButton(
-              onPressed: () {
-                picker.getImage(source: ImageSource.gallery);
+              onPressed: () async {
+                final PickedFile file =
+                    await picker.getImage(source: ImageSource.gallery);
+                onImageSelected(File(file.path));
               },
               child: const Text('Galeria'),
             )
